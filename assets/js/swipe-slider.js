37c6a3dc3e9ad69ab4ee42d2a2971e8a20e32dd3
muntasirudoy <muntasirudoy4@gmail.com>
2021-12-10 16:44:51 +0600

    ecommerce

--- assets/js/swipe-slider.js
+++ assets/js/swipe-slider.js
@@ -5,13 +5,11 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
     loop: true,
     coverflowEffect: {
-      rotate: 0,
+      rotate: 15,
       stretch: 0,
-      depth: 100,
+      depth: 200,
       modifier: 1,
       slideShadows: false
     },
-    pagination: {
-      el: ".swiper-pagination"
-    }
+    
   });
